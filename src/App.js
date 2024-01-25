7cc8907ad65836fd6aa81ff489f2261c618c14b9
Alicia Mendez Cruz <aliciamendezcruz@alichus-mbp-16.home>
2024-01-25 19:40:05 +0100

    news class and props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Button from "./components/button";
+import News from "./components/news";
 
 class App extends Component {
   //state = {  }
@@ -11,6 +12,8 @@ class App extends Component {
       <div>
         <Header />
         <Main />
+        <News news="Great news everyone!" />
+        <News news="Oh no! Niffler!" />
         <Button text={"Button 1"} class="important" />
         <Button text={"Button 2"} class="danger" />
         <Button text={"Button 3"} class="allClear" />
