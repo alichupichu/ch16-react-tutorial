f04dfb2331161a69c81ccc3675f3736fe7fcb2ae
Alicia Mendez Cruz <aliciamendezcruz@Alichus-MacBook-Pro-16.local>
2024-02-08 19:07:23 +0000

    counter functional react

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 
 const App = () => {
+  //bellow is short hand
+  const [count, setCount] = useState({ count: 0 });
+
+  //long way below
+
+  // const myState = useState(0);
+  // const count = myState[0];
+  // const setCount = myState[1]
+
+  const add = () => {
+    setCount(count + 1);
+  };
+
+  const minus = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div>
-      <Header />
-      <aside></aside>
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <button onClick={add}>+</button>
+      <button onClick={minus}>-</button>
+      <p>Count : {count}</p>
+    </>
   );
 };
 
